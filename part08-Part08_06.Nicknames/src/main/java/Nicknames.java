491b50dee6b0b8bccbb84c30bdf1b90711dbb2db
DavidMplay <vudavidmadar@gmail.com>
2023-12-05 18:21:18 +0100

    Completed part 8 exercise 6

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -4,7 +4,11 @@ import java.util.HashMap;
 public class Nicknames {
 
     public static void main(String[] args) {
-        // Do the operations required here!
+        HashMap<String, String> nicknames = new HashMap<>();
+        nicknames.put("matt", "matthew");
+        nicknames.put("mix", "michael");
+        nicknames.put("artie", "arthur");
+        System.out.println(nicknames.get("matt"));
     }
 
 }
