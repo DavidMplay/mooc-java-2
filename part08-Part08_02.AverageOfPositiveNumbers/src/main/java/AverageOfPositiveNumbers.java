3abe737a0d7f21b3cc1baa325c081231c523b32f
DavidMplay <vudavidmadar@gmail.com>
2023-12-05 17:54:25 +0100

    Completed part 8 exercises 2 and 3

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,21 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int sum = 0;
+        int count = 0;
+        while (true){
+            int num = Integer.valueOf(scanner.nextLine());
+            if(num == 0){
+                break;
+            }
+            if(num > 0){
+                sum += num;
+                count++;
+            }
+        }
+        if(sum == 0){
+            System.out.println("Cannot calculate the average");
+        }
+        System.out.println((double)sum / count);
     }
 }
--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,8 +5,8 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        int container1 = 0;
+        int container2 = 0;
         while (true) {
             System.out.print("> ");
 
@@ -14,8 +14,42 @@ public class LiquidContainers {
             if (input.equals("quit")) {
                 break;
             }
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
 
+            System.out.println("First: " + container1 + "/" + 100);
+            System.out.println("Second: " + container2 + "/" + 100);
+            if (command.equals("add") && amount >= 0) {
+                if (container1 + amount > 100) {
+                    container1 = 100;
+                } else {
+                    container1 += amount;
+                }
+            } else if (command.equals("move") && amount >= 0) {
+                if ((container1 - amount) >= 0) {
+                    container2 += amount;
+                    container1 -= amount;
+                    if (container2 >= 100) {
+                        container2 = 100;
+                    }
+                } else if ((container1 - amount) < 0) {
+                    container2 += container1;
+                    container1 = 0;
+                    if (container2 >= 100) {
+                        container2 = 100;
+                    }
+                }
+            } else if (command.equals("remove") && amount >= 0) {
+                if (container2 - amount >= 0) {
+                    container2 -= amount;
+                } else if (container2 - amount < 0) {
+                    container2 = 0;
+                }
+            }
         }
-    }
 
+        System.out.println("First: " + container1 + "/" + 100);
+        System.out.println("Second: " + container2 + "/" + 100);
+    }
 }
