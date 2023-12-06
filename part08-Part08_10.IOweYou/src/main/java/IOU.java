ee0297e7343f5c89341cc60eb468a33abfd0fdf9
DavidMplay <vudavidmadar@gmail.com>
2023-12-06 16:25:41 +0100

    Completed part 8 exercise 10

---
+++ part08-Part08_10.IOweYou/src/main/java/IOU.java
@@ -0,0 +1,21 @@
+
+import java.util.HashMap;
+
+public class IOU {
+
+    private HashMap<String, Double> list;
+    
+    public IOU() {
+        this.list = new HashMap<>();
+    }
+
+    public void setSum(String toWhom, double amount) {
+        this.list.put(toWhom, amount);
+    }
+
+    public double howMuchDoIOweTo(String toWhom) {
+        if (this.list.keySet().contains(toWhom)) {
+            return this.list.get(toWhom);
+        } return 0;
+    }
+}
