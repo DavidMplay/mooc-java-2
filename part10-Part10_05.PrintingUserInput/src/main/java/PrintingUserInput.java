8466d65ebf692379ac20ec8ad0c6efbc215c404c
DavidMplay <vudavidmadar@gmail.com>
2023-12-14 15:05:39 +0100

    Completed part 10 exercise 5

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,15 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList();
 
+        while (true) {
+            String s = scanner.nextLine();
+            if (s.isEmpty()){
+                break;
+            }
+            list.add(s);
+        }
+        list.stream().forEach(sentance -> System.out.println(sentance));
     }
 }
