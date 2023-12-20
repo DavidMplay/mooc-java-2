1c119a737f82b3ba40e4cab68152a4f8498460b6
DavidMplay <vudavidmadar@gmail.com>
2023-12-20 16:02:36 +0100

    Completed part 11 exercise 5

---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,9 @@
+public class Bot extends Player{
+    
+    public void play(){
+        System.out.println("Play!!");
+    }
+    public void addMove(String move){
+        
+    }
+}
---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,14 @@
+
+public class Player {
+    private String name;
+    
+    public Player(){
+        
+    }
+    public void play(){
+        System.out.println("Playing!");
+    }
+    public void printNmae(){
+        System.out.println(this.name);
+    }
+}
