71b287755fc58a6213f61f52a2462c9657585a30
DavidMplay <vudavidmadar@gmail.com>
2024-01-04 07:57:16 +0100

    Completed part 12 exercise 1

---
+++ part12-Part12_01.Hideout/src/main/java/Hideout.java
@@ -0,0 +1,22 @@
+
+public class Hideout<T> {
+
+    private T hidden;
+
+    public Hideout() {
+
+    }
+
+    public void putIntoHideout(T toHide) {
+        hidden = toHide;
+    }
+
+    public T takeFromHideout() {
+        if (this.hidden == null) {
+            return null;
+        }
+        T taken = this.hidden;
+        this.hidden = null;
+        return taken;
+    }
+}
