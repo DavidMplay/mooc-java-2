3d58969062cf3415d9165ce5382d6c2e9cd48ff0
DavidMplay <vudavidmadar@gmail.com>
2024-01-20 10:41:10 +0100

    Completed part 13 exercise 6

--- part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,32 @@
 package notifier;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
+public class NotifierApplication extends Application{
 
-public class NotifierApplication {
-
+    public void start(Stage window){
+        VBox layout = new VBox();
+        Button button = new Button("Update");
+        TextField textField = new TextField();
+        Label label = new Label();
+        button.setOnAction((event)->{
+            label.setText(textField.getText());
+        });
+        layout.getChildren().addAll(textField, button, label);
+        
+        Scene scene = new Scene(layout);
+        
+        window.setScene(scene);
+        window.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NotifierApplication.class);
     }
 
 }
